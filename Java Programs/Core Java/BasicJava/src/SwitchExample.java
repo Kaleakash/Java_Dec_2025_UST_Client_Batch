861348cd4d3e7aa2e25fbb85b50c39c02789e67f
Akash <akash300383@gmail.com>
2025-12-16 19:34:23 +0530

    done

---
+++ Java Programs/Core Java/BasicJava/src/SwitchExample.java
@@ -0,0 +1,26 @@
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int choice=100;
+		switch(choice) {
+		case 1: System.out.println("block1");
+				break;
+
+		case 2:System.out.println("block2");
+		        break;
+		        
+		case 3:System.out.println("block3");
+        			break;
+        
+		case 4:System.out.println("block4");
+        			break;
+        	
+        	default : System.out.println("wrong choice");
+        	         break;
+		}
+		System.out.println("Normal Statement");
+	}
+
+}
