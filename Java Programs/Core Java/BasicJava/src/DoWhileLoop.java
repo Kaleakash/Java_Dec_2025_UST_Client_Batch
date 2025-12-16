2538b83403dde99fc2da5d9b14eafbaf6d6e66e2
Akash <akash300383@gmail.com>
2025-12-16 20:42:38 +0530

    done

---
+++ Java Programs/Core Java/BasicJava/src/DoWhileLoop.java
@@ -0,0 +1,15 @@
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1,n=100;
+		
+		do {
+			System.out.println("i ="+i);
+			i++;
+		}while(i>n);
+		System.out.println("Finish");
+	}
+
+}
---
+++ Java Programs/Core Java/BasicJava/src/WhileLoopExample.java
@@ -0,0 +1,16 @@
+
+public class WhileLoopExample {
+
+	public static void main(String[] args) {
+		
+		int i=1,n=100;
+		
+		while(i<=n) {
+			//System.out.println("Hello");
+			System.out.println("i ="+i);
+			i++;
+		}
+		System.out.println("Finish");
+	}
+
+}
