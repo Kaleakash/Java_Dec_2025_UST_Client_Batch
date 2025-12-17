e6f94c7c1b7b67f96a8c3925ef57a5186898a355
Akash <akash300383@gmail.com>
2025-12-17 19:05:08 +0530

    done

--- Java Programs/Core Java/BasicJava/src/RunTimeValueForArray.java
+++ Java Programs/Core Java/BasicJava/src/RunTimeValueForArray.java
@@ -5,11 +5,14 @@ public class RunTimeValueForArray {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("How many element do you want to store");
 		int n = sc.nextInt();
+		// int num[]=new int[5]; int num[10] invalid in java 
 		int num[]=new int[n];		// dynamic memory created.
 		System.out.println("Enter the number one by one");
 		for(int i=0;i<num.length;i++) {
 			num[i]=sc.nextInt();
 		}
+		// apply logic. 
+		
 		System.out.println("all array element are ");
 		for(int i=0;i<num.length;i++) {
 			System.out.print(" "+num[i]);
---
+++ Java Programs/Core Java/BasicJava/src/TwoDimensionalArray.java
@@ -0,0 +1,20 @@
+
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+		int num[][]= 
+			{
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+			};
+		System.out.println("display 2 - D array");
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				System.out.print(num[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
