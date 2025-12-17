c7f67f68daf9721f68bd8ee5b2c5761c5aaa9f14
Akash <akash300383@gmail.com>
2025-12-17 18:52:41 +0530

    done

---
+++ Java Programs/Core Java/BasicJava/src/RunTimeValueForArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class RunTimeValueForArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How many element do you want to store");
+		int n = sc.nextInt();
+		int num[]=new int[n];		// dynamic memory created.
+		System.out.println("Enter the number one by one");
+		for(int i=0;i<num.length;i++) {
+			num[i]=sc.nextInt();
+		}
+		System.out.println("all array element are ");
+		for(int i=0;i<num.length;i++) {
+			System.out.print(" "+num[i]);
+		}
+		sc.close();
+	}
+
+}
