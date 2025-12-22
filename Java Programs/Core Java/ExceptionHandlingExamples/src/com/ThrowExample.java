5f39b352baf101c49c9642ce20811616eb093532
Akash <akash300383@gmail.com>
2025-12-22 19:31:08 +0530

    done

---
+++ Java Programs/Core Java/ExceptionHandlingExamples/src/com/ThrowExample.java
@@ -0,0 +1,23 @@
+package com;
+
+import java.util.Scanner;
+
+public class ThrowExample {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the age");
+		int age = sc.nextInt();
+		try {
+		if(age<21) {
+			//throw new Exception();		throw generic exception 
+			throw new Exception("age must be >21");
+		}else {
+			System.out.println("You can Vote!");
+		}
+		}catch(Exception e) {
+			System.err.println(e.toString());
+		}
+	}
+
+}
